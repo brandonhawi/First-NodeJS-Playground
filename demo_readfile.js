406dcd8a894b8d38750f332cf92b1d4b9824dc5e
Brandon Hawi <brandonhawi@jasonbusiness.com>
2019-07-15 13:49:13 -0700

    Edited the path to the guessing game file

--- demo_readfile.js
+++ demo_readfile.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 http.createServer(function(req,res)
 {
-    fs.readFile('../GuessingGame/number-guessing-game-start.html', function(err, data) {
+    fs.readFile('./GuessingGame/number-guessing-game-start.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
